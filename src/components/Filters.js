4d0c041e32f54380b8929279ed0c332ccea051ca
pythagoras58 <solomondanso58@gmail.com>
2022-05-21 22:35:14 +0000

    First Commit

---
+++ src/components/Filters.js
@@ -0,0 +1,91 @@
+import { Button, Form } from "react-bootstrap";
+import { CartState } from "../context/Context";
+import Rating from "./Rating";
+
+const Filters = () => {
+  const {
+    productDispatch,
+    productState: { byStock, sort, byRating },
+  } = CartState();
+
+  // make state for rating
+
+  return (
+    <div className="filters">
+      <span className="title">FILTER</span>
+      <span>
+        <Form.Check
+          inline
+          label="Low Price"
+          name="group1"
+          type="radio"
+          id={`inline-1`}
+          onChange={() =>
+            productDispatch({
+              type: "SORT_BY_PRICE",
+              payload: "lowToHigh",
+            })
+          }
+          checked={sort === "lowToHigh" ? true : false}
+        />
+      </span>
+      <span>
+        <Form.Check
+          inline
+          label="High Price"
+          name="group1"
+          type="radio"
+          id={`inline-2`}
+          onChange={() =>
+            productDispatch({
+              type: "SORT_BY_PRICE",
+              payload: "highToLow",
+            })
+          }
+          checked={sort === "highToLow" ? true : false}
+        />
+      </span>
+      <span>
+        <Form.Check
+          inline
+          label="Include Out of Stock"
+          name="group1"
+          type="checkbox"
+          id={`inline-3`}
+          onChange={() =>
+            productDispatch({
+              type: "FILTER_BY_STOCK",
+            })
+          }
+          checked={byStock}
+        />
+      </span>
+     
+      <span>
+        <label style={{ paddingRight: 10 }}>Rating: </label>
+        <Rating
+          rating={byRating}
+          onClick={(i) =>
+            productDispatch({
+              type: "FILTER_BY_RATING",
+              payload: i + 1,
+            })
+          }
+          style={{ cursor: "pointer" }}
+        />
+      </span>
+      <Button
+        variant="light"
+        onClick={() =>
+          productDispatch({
+            type: "CLEAR_FILTERS",
+          })
+        }
+      >
+        Clear Filters
+      </Button>
+    </div>
+  );
+};
+
+export default Filters;
